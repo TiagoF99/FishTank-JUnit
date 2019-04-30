b3f3a1e10a9a30764839102bb34e3c2c16f1deee
Tiago Ferreira <45250015+TiagoF99@users.noreply.github.com>
2019-04-29 21:24:33 -0400

    Update FishTest.java

--- src/test/java/fishtank/FishTest.java
+++ src/test/java/fishtank/FishTest.java
@@ -8,14 +8,7 @@ import static org.junit.Assert.assertTrue;
 
 public class FishTest {
 
-    /* Note: FishTest is in the package fish tank, so it has access to package
-       private attributes.
-
-       Also note: It's *vital* that you name any other test classes
-       (ClassName)Test in the same directory as this one is in.
-       properly capitalized -- we will be auto grading your tests, so make sure
-       to follow this naming convention!
-     */
+   
     private Fish fish;
 
     @Before
@@ -61,24 +54,18 @@ public class FishTest {
 
     @Test
     public void testFishBubbles() {
-        //Note: This test currently fails, but should pass once you've
-      // refactored &
-        //fixed the starter code.
         boolean bubbleMade = false;
         for (int i = 0; i < 200; i++) {
             fish.setLocation(5, 10);
             fish.goingRight =
-                false; //notice: I can edit package private attributes!
+                false;
             fish.update();
-            //fish should move one tile left and eventually blow a bubble.
             FishTankEntity e = FishTank.getEntity(5, 10);
             if (e instanceof Bubble) {
                 bubbleMade = true;
                 break;
             }
         }
-        //You could also write "assert bubbleMade", but using the JUnit version
-        //makes the message much nicer if it fails.
         assertTrue(bubbleMade);
     }
 
